522573502573f4c50865e38ece7f8fcfb0e0082a
Doug Burke <dburke@cfa.harvard.edu>
2022-06-24 15:07:51 -0400

    Use niv

--- default.nix
+++ default.nix
@@ -1,11 +1,3 @@
-{ mkDerivation, base, directory, filepath, stdenv, unix }:
-mkDerivation {
-  pname = "mydu";
-  version = "1.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [ base directory filepath unix ];
-  description = "A basic recursive du";
-  license = stdenv.lib.licenses.publicDomain;
-}
+{ compiler ? "ghc8107"
+}:
+(import ./release.nix { compiler = compiler; }).exe
